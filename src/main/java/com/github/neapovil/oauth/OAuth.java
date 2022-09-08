51a2182b81c2f789ccf02c1906547d9147722e58
William <nearata@protonmail.com>
2022-09-08 14:48:42 +0200

    validate codes

--- src/main/java/com/github/neapovil/oauth/OAuth.java
+++ src/main/java/com/github/neapovil/oauth/OAuth.java
@@ -3,16 +3,19 @@ package com.github.neapovil.oauth;
 import java.io.File;
 import java.security.SecureRandom;
 import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent.Result;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.electronwill.nightconfig.core.file.FileConfig;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import net.kyori.adventure.text.Component;
 import spark.Spark;
@@ -22,14 +25,19 @@ public final class OAuth extends JavaPlugin implements Listener
     private static OAuth instance;
     private FileConfig config;
     private String secret = "";
-    private final Map<UUID, Auth> codes = new HashMap<>();
+    private final Map<String, Auth> codes = new ConcurrentHashMap<>();
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     @Override
     public void onEnable()
     {
         instance = this;
-        
+
         this.getServer().getPluginManager().registerEvents(this, this);
+
+        this.getServer().getScheduler().runTaskTimerAsynchronously(this, () -> {
+            this.codes.values().removeIf(i -> Instant.now().isAfter(i.time));
+        }, 0, 20);
 
         this.saveResource("config.json", false);
 
@@ -45,29 +53,64 @@ public final class OAuth extends JavaPlugin implements Listener
             final String secret = this.generateSecret();
             this.config.set("secret", secret);
         }
-        
+
         this.secret = this.config.get("secret");
-        
+
         final int port = this.config.getInt("port");
-        
+
         Spark.port(port);
-        
+
         Spark.before((req, res) -> {
+            res.type("application/json");
+
             final String secret = req.headers("secret");
-            
+
+            final ResponseError response = new ResponseError("");
+
             if (secret == null)
             {
-                Spark.halt(404);
+                Spark.halt(404, this.gson.toJson(response));
             }
-            
+
             if (!secret.equals(this.secret))
             {
-                Spark.halt(404);
+                Spark.halt(404, this.gson.toJson(response));
+            }
+
+            RequestBody data = null;
+
+            try
+            {
+                data = this.gson.fromJson(req.body(), RequestBody.class);
+            }
+            catch (Exception e)
+            {
+                Spark.halt(400, this.gson.toJson(response));
+            }
+
+            final Auth auth = this.codes.get(data.ip);
+
+            if (auth == null || data.code != auth.code || Instant.now().isAfter(auth.time))
+            {
+                response.message = "INVALID CODE";
+                Spark.halt(400, this.gson.toJson(response));
             }
         });
-        
-        Spark.get("/verify", (req, res) -> {
-            return "Hello World!";
+
+        Spark.get("/verify", "application/json", (req, res) -> {
+            res.type("application/json");
+
+            final RequestBody data = this.gson.fromJson(req.body(), RequestBody.class);
+            final Auth auth = this.codes.remove(data.ip);
+
+            if (auth == null)
+            {
+                Spark.halt(400, this.gson.toJson(new ResponseError("EXPIRED")));
+            }
+
+            final ResponseSuccess response = new ResponseSuccess(auth.username, auth.avatar);
+
+            return this.gson.toJson(response);
         });
     }
 
@@ -96,35 +139,95 @@ public final class OAuth extends JavaPlugin implements Listener
                 .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
                 .toString();
     }
-    
+
     private int generateCode()
     {
         final SecureRandom random = new SecureRandom();
-        
+
         return random.nextInt(100000, 1000000);
     }
-    
+
     @EventHandler
     private void AsyncPlayerPreLogin(AsyncPlayerPreLoginEvent event)
     {
+        final String ip = event.getAddress().getHostAddress();
         final UUID id = event.getUniqueId();
-        final Auth auth = this.codes.get(id);
-        
-        if (Instant.now().isAfter(auth.time))
+        final Auth auth = this.codes.get(ip);
+
+        if (auth == null || Instant.now().isAfter(auth.time))
         {
-            event.kickMessage(Component.text(""));
+            final int code = this.generateCode();
+
+            this.codes.put(ip, new Auth(code, Instant.now().plusSeconds(60), ip, id, event.getName()));
+
+            event.disallow(Result.KICK_OTHER, Component.text(this.formatCode(code)));
+
+            return;
         }
+
+        event.disallow(Result.KICK_OTHER, Component.text(this.formatCode(auth.code)));
     }
-    
+
+    private String formatCode(int s)
+    {
+        final StringBuilder sb = new StringBuilder("" + s);
+
+        sb.insert(3, " ");
+
+        return sb.toString();
+    }
+
     class Auth
     {
-        public final String code;
+        public final int code;
         public final Instant time;
-        
-        public Auth(String code, Instant time)
+        public final String address;
+        public final UUID id;
+        public final String username;
+        public final String avatar;
+
+        public Auth(int code, Instant time, String address, UUID id, String username)
         {
             this.code = code;
             this.time = time;
+            this.address = address;
+            this.id = id;
+            this.username = username;
+            this.avatar = "https://crafatar.com/avatars/" + id;
+        }
+    }
+
+    class RequestBody
+    {
+        public final int code;
+        public final String ip;
+
+        public RequestBody(int code, String ip)
+        {
+            this.code = code;
+            this.ip = ip;
+        }
+    }
+
+    class ResponseError
+    {
+        public String message;
+
+        public ResponseError(String message)
+        {
+            this.message = message;
+        }
+    }
+
+    class ResponseSuccess
+    {
+        public String username;
+        public String avatar;
+
+        public ResponseSuccess(String username, String avatar)
+        {
+            this.username = username;
+            this.avatar = avatar;
         }
     }
 }
